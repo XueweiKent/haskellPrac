4d407b3296ab1e976b9bfa3c24f103e0429ab976
XueweiKent <kentxuewei@gmail.com>
2016-01-22 15:51:57 -0600

    finished exercise

--- demo/2RecursionActivity/critic.hs
+++ demo/2RecursionActivity/critic.hs
@@ -10,7 +10,14 @@ removeNegatives (x:xs) | x < 0 = result
                        | otherwise = x : result
   where result = removeNegatives xs
 
---3. illegal name
+--3. no efficient? 
 reverse' [] = []
-reverse' (x:xs) = trace (show xs) (reverse xs) ++ [x]
+reverse' (x:xs) = trace (show xs) (reverse' xs) ++ [x]
 
+reverse'' xx = aux xx [] 
+  where aux [] a     = a
+        aux (x:xs) a = aux xs (x:a)
+
+--4. a. base case first. b. induction
+decList [] = []
+decList (x:xs) = (x-1) : (decList xs)
--- demo/2RecursionActivity/tailify.hs
+++ demo/2RecursionActivity/tailify.hs
@@ -1,3 +1,5 @@
+import Debug.Trace
+
 maxList [xs] = aux xs (head xs)
   where 
   	aux [] acc = acc
@@ -24,4 +26,31 @@ lchop heads = aux heads (length heads)
   	aux [x] level = [x-1]
   	aux (0:xs) level = 0 : aux xs (level - 1)
   	aux (x0:x1:xs) level = x0 - 1 : x1 + level - 1 : xs
-  	
+
+
+maxL (x:xs) = aux xs x
+  where 
+  	aux [] acc = acc
+  	aux (y:ys) acc = aux ys (max y acc)
+
+myFib n = aux 1 1 n
+  where 
+  	aux a b 1 = a
+  	aux a b n = aux b (a+b) (n-1)
+
+bigChop heads = aux heads 0
+  where 
+  	aux [] carry = carry
+  	aux (x:xs) carry = aux xs ((carry+x) * (length (x:xs)))
+
+smallChop (x:xs) = aux x xs 
+  where 
+    elimZero (x:xs) = if(x==0) then xs else (x:xs)
+    aux 0 (y:ys) = aux y ys
+    aux x []     = if (x>0) then [x-1] else [0]
+    aux x (y:ys) = elimZero ( (x-1):(y+length(ys)+1):ys )
+
+countSmallChop xx = aux xx 0
+  where 
+    aux [0] n = n
+    aux xx  n = aux (smallChop xx) n+1
