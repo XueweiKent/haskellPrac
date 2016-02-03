c7bf8e72c67e6fb07739df3fae967906bef53251
XueweiKent <kentxuewei@gmail.com>
2016-02-03 15:00:45 -0600

    done with activities

--- demo/5LambdaCalculus/lambda.hs
+++ demo/5LambdaCalculus/lambda.hs
@@ -9,3 +9,21 @@ subst n x orig@(Var v) | v /= x   = orig
 subst n x (App m p) = App (subst n x m) (subst n x p)
 subst n x orig@(Abs v m) | v==x  = orig
                          | otherwise = Abs v (subst n x m)
+
+
+show m = m (+1) 0
+
+numOccurs :: LExp -> LExp -> Int
+numOccurs (Var u) (Var v) 
+   | v==u = 1
+   | otherwise = 0
+numOccurs (Abs str exp) var@(Var v) = 
+	numOccurs exp var
+numOccurs (App exp1 exp2) var@(Var v) = 
+    numOccurs exp1 var + (numOccurs exp2 var)
+
+
+distictVars :: LExp -> Int
+distictVars (Var str) = 0
+distictVars (App exp1 exp2) = distictVars exp1 + (distictVars exp2)
+distictVars (Abs str exp) = 1 + distictVars exp
